b97fb2db89fabb4c5962bcca1be73030e09c61f7
soyeon1026 <asdfgrace1532@gmail.com>
2026-02-10 07:59:34 +0000

    기본출력 위젯

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.title("Hello Streamlit")
+st.title("안녕하세요 Streamlit")
+
+st.header("헤더를 입력할 수 있어요 :sunglasses:")
+
+st.subheader("이것은subheader입니다")
+
+st.text("일반적인 텍스트 출력")
+
+st.caption("캡션을 넣어봅니다")
+
+sample_code = '''
+def function():
+    print('hello world')
+'''
+
+st.code(sample_code)
